21aa21c26d59bb2741dfff12a46b2de79cfdddee
Billy Wellington <111053029+billywellington@users.noreply.github.com>
2023-05-19 09:18:08 +0200

    Create subtract.c

---
+++ 0xcalculator/functions/subtract.c
@@ -0,0 +1,12 @@
+#include "calc.h"
+
+/**
+ * diff - calculates the difference of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the difference of a and b
+ */
+double diff(double a, double b){
+    return (a - b);
+}
