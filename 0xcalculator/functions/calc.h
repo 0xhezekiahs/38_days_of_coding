39b9d4a0a0c30a3e530bd789be4d306b308a5eb0
Billy Wellington <billierangwani@gmail.com>
2023-05-19 17:15:06 +0200

    update on div prototype

--- 0xcalculator/functions/calc.h
+++ 0xcalculator/functions/calc.h
@@ -5,8 +5,7 @@
 
 double sum(double a, double b);
 double diff(double a, double b);
-double diff(double a, double b);
 double mul(double a, double b);
-double div(double a, double b, *error);
+double div(double a, double b);
 
 #endif
