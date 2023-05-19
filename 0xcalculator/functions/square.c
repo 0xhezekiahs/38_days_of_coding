281285195e124212440c1dbf18c21d7c83961735
Billy Wellington <billierangwani@gmail.com>
2023-05-19 18:00:41 +0200

    adding suqare function

---
+++ 0xcalculator/functions/square.c
@@ -0,0 +1,15 @@
+#include "calc.h"
+
+/**
+ * diff - calculates the square of a number
+ * @a: first number
+ *
+ * Return: the square of a 
+ */
+double square(double a){
+    if (a<=0){
+        printf("Enter a positive number")
+      //  scanf("%lf", );
+    }
+    return (a*a);
+}
