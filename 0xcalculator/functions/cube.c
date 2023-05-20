cc30243518187ccaf0df7cd1d793f1875104cad8
0xhezekiahs <preshnwagbo@gmail.com>
2023-05-20 18:44:29 -0400

    cube

---
+++ 0xcalculator/functions/cube.c
@@ -0,0 +1,14 @@
+#include "calc.h"
+
+/**
+ * cube - compute the cube of a number
+ * @n: number
+ * 
+ * Return: the cube
+ */
+double cube(double a)
+{
+	double result = a * a * a;
+
+	return (result);
+}
