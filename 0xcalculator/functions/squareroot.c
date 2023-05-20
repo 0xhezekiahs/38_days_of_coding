4f8962a4897cea2e6633b9cb3fcc77d4108522b4
Billy Wellington <billierangwani@gmail.com>
2023-05-20 21:13:56 +0200

    adding the square root function

---
+++ 0xcalculator/functions/squareroot.c
@@ -0,0 +1,14 @@
+#include "calc.h"
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * sq_root - calculates the square of a number
+ * @a: the input from user
+ *
+ * Return: the square root of a 
+ */
+
+double sq_root(double a){
+    return (sqrt(a));
+}
