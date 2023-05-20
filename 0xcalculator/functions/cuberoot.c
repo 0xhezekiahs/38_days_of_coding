09bcbbbc49788d8aef19237cd5450dd1a9d853f5
0xhezekiahs <preshnwagbo@gmail.com>
2023-05-20 18:53:12 -0400

    cube root

---
+++ 0xcalculator/functions/cuberoot.c
@@ -0,0 +1,13 @@
+#include "calc.h"
+/**
+ * cuberoot - computd the cube root of a number
+ * @n: number
+ *
+ * Return: result
+ */
+double cuberoot(double n)
+{
+	double result = cbrt(n);
+
+	return (result);
+}
