ebf12dde1cf4cbe46a8da51ea2be0619eac901c6
0xhezekiahs <preshnwagbo@gmail.com>
2023-05-22 23:33:05 +0100

    Prototypes for normal trig functions

--- 0xcalculator/functions/calc.h
+++ 0xcalculator/functions/calc.h
@@ -11,4 +11,8 @@ double square(double a);
 double sq_root(double a);
 double cube(double n);
 double cuberoot(double n);
+double sin(double n);
+double cos(double n);
+double tan(double n);
+
 #endif
